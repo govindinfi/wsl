44032b3feb277096661428da98b1f1489c51ba28
Govind Kumar <102216439+govindinfi@users.noreply.github.com>
2023-04-05 17:33:54 +0530

    New changes

--- grub_passwd.sh
+++ grub_passwd.sh
@@ -3,8 +3,10 @@
 # Govind Kumar 
 # Set the GRUB2 password 
 # ----------------------------------------------------------------
+set physical
+
 echo "Setting GRUB2 password..."
-grub2-mkpasswd-pbkdf2 | tee /etc/grub2/grub.pbkdf2.sha512
-echo "set superusers=\"root\"" >> /etc/grub2.cfg
-echo "password_pbkdf2 root $(cat /etc/grub2/grub.pbkdf2.sha512)" >> /etc/grub2.cfg
-echo "Done."
+grub2-mkpasswd-pbkdf2 | tee /tmp/grub.pbkdf2.sha512
+echo "set superusers=\"root\"" >> /etc/grub.d/40_custom
+echo "password_pbkdf2 root $(cat /tmp/grub.pbkdf2.sha512 | tail -n 1)" >> /etc/grub.d/40_custom
+grub2-mkconfig -o /boot/grub2/grub.cfg   # Update the GRUB configuration
